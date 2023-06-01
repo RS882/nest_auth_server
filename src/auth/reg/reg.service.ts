e0a8254ab851738c753cc5ed2d601e9aae2c1b58
Russen <russen333@gmail.com>
2023-06-01 10:10:04 +0300

    add login func

--- src/auth/reg/reg.service.ts
+++ src/auth/reg/reg.service.ts
@@ -12,6 +12,7 @@ import { UserDTO } from '../DTO/user.dto';
 import { APIUserDTO } from '../DTO/apiUser.dto';
 import { Token_Nest_Auth } from 'src/postgre-sql/entity/token.entity';
 import { MailService } from '../mail/mail.service';
+import { env } from 'process';
 
 
 
@@ -28,8 +29,20 @@ export class RegService {
     private mailService: MailService,
   ) { }
 
+  async #getAPIUserDTO(data: User_Nest_Auth, isNeedToSendMail = false): Promise<APIUserDTO> {
+    const userRegData: UserDTO = { id: data.id, email: data.email, isActivate: data.is_activated }
+    const tokens: TokensDTO = this.tokenService.generateTokens(userRegData)
+    const createdRefreshToken: Token_Nest_Auth = this.tokenRepository.create({ user_id: data, refresh_token: tokens.refreshToken })
+    await this.tokenRepository.save(createdRefreshToken)
 
-  async reg(user: RegUserDTO, isNeedToSendMail = false): Promise<APIUserDTO> {
+    if (isNeedToSendMail) await this.mailService.sendActivationLink(
+      data.email,
+      `${env.API_URL}/auth/activate/${data.activation_link}`);
+
+    return { ...tokens, user: userRegData };
+  }
+
+  async reg(user: RegUserDTO): Promise<APIUserDTO> {
 
     const findEmail: User_Nest_Auth[] = await this.usersRepository.findBy({ email: user.userEmail })
     if (findEmail.length > 0) throw new BadRequestException(`The user with email ${user.userEmail} has already been registered`)
@@ -40,42 +53,19 @@ export class RegService {
     const createdNewUser: User_Nest_Auth = this.usersRepository.create({ email: user.userEmail, pasword: hashPass, activation_link: uuidActivationLink });
     const savedNewUser: User_Nest_Auth = await this.usersRepository.save(createdNewUser)
 
-    const userRegData: UserDTO = { id: savedNewUser.id, email: savedNewUser.email, isActivate: savedNewUser.is_activated }
-    const tokens: TokensDTO = this.tokenService.generateTokens(userRegData)
-    const createdRefreshToken: Token_Nest_Auth = this.tokenRepository.create({ user_id: savedNewUser, refresh_token: tokens.refreshToken })
-    await this.tokenRepository.save(createdRefreshToken)
-
-    if (isNeedToSendMail) await this.mailService.sendActivationLink(savedNewUser.email, savedNewUser.activation_link);
-
-    return { ...tokens, user: userRegData };
+    return this.#getAPIUserDTO(savedNewUser)
 
 
   }
 
   async login(user: RegUserDTO): Promise<APIUserDTO> {
     const findEmail: User_Nest_Auth[] = await this.usersRepository.findBy({ email: user.userEmail })
-
-
     if (findEmail.length === 0) throw new BadRequestException(`User is not found`)
-
     if (findEmail.length > 1) throw new BadRequestException(`User data is duplicated`)
-
-    const findUser = findEmail[0];
-
-
-    const isPasswordEquil: boolean = await bcrypt.compare(user.userPassword, findUser.pasword);
+    const isPasswordEquil: boolean = await bcrypt.compare(user.userPassword, findEmail[0].pasword);
     if (!isPasswordEquil) throw new BadRequestException(`Incorrect password`)
 
-
-    const userRegData: UserDTO = { id: findUser.id, email: findUser.email, isActivate: findUser.is_activated }
-    const tokens: TokensDTO = this.tokenService.generateTokens(userRegData)
-    const createdRefreshToken: Token_Nest_Auth = this.tokenRepository.create({ user_id: findUser, refresh_token: tokens.refreshToken })
-    await this.tokenRepository.save(createdRefreshToken)
-
-
-
-    return { ...tokens, user: userRegData };
-
+    return this.#getAPIUserDTO(findEmail[0])
   }
 
 
